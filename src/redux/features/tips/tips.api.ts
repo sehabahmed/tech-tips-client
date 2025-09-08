5aa4fc6cd651925916b408a70281d3879cb91d30
Sehab Ahmed <sehabahmed50100@gmail.com>
2025-09-08 10:10:54 +0200

    fix: tips api endpoints bug fixed and TResponseRedux added

---
+++ src/redux/features/tips/tips.api.ts
@@ -0,0 +1,24 @@
+import { baseApi } from "@/redux/api/baseApi";
+import { TResponseRedux } from "@/types/global.type";
+
+const tipsApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    tipsPost: builder.mutation({
+      query: (data) => ({
+        url: "/tips",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    getAllTips: builder.query({
+      query: () => ({
+        url: "/tips",
+        method: "GET",
+      }),
+      providesTags: ["tips"],
+      transformResponse: (response: TResponseRedux<any>) => {
+        return { data: response.data };
+      },
+    }),
+  }),
+});
---
+++ src/types/global.type.ts
@@ -0,0 +1,19 @@
+import { BaseQueryApi } from "@reduxjs/toolkit/query";
+
+export type TError = {
+    data: {
+        message: string;
+        stack: string;
+        success: boolean;
+    }
+    status: number;
+}
+
+export type TResponse<T> = {
+    data?: T;
+    error?: TError;
+    success: boolean;
+    message: string;   
+}
+
+export type TResponseRedux<T> = TResponse<T> & BaseQueryApi;
